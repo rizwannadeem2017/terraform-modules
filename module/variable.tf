0b7e04331fe685d55728f74d5c8d6e5c974ad0a7
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-20 18:39:46 +0500

    ads

---
+++ module/variable.tf
@@ -0,0 +1,21 @@
+variable "access_key" {
+
+}
+
+variable "secret_key" {
+
+}
+
+variable "region" {
+  default = ""
+}
+
+variable "environment" {}
+
+variable "tags" {}
+
+variable "instance_count" {}
+
+variable "instance_type" {}
+
+variable "ami" {}
