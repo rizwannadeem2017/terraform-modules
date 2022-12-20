b81bc7cb7637e24a0622243b9b4419cd3381f4a7
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-20 21:00:58 +0500

    ad

--- module/variable.tf
+++ module/variable.tf
@@ -1,10 +1,10 @@
-variable "access_key" {
-  default = ""
-}
+# variable "access_key" {
+#   default = ""
+# }
 
-variable "secret_key" {
-  default = ""
-}
+# variable "secret_key" {
+#   default = ""
+# }
 
 variable "region" {
   default = ""
