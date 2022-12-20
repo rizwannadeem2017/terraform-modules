c7e9917378cfb10609f71d64c48e423c8d00b0b3
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-20 15:36:51 +0500

    asdads

--- test/variable.tf
+++ test/variable.tf
@@ -1,16 +1,17 @@
 variable "access_key" {
+  default = ""
 }
 
 variable "secret_key" {
-
+  default = ""
 }
 
 variable "region" {
-
+  default = ""
 }
 
 variable "environment" {
-
+  default = ""
 }
 
 variable "tags" {
