f4dcc25494bd843f2dbbc27992fc97de265fb468
Rizwan Nadeem <m_rizwan6@hotmail.com>
2022-12-27 12:28:57 +0500

    Delete module directory

--- module/README.md
+++
@@ -1,6 +0,0 @@
-# terraform-Infra
-
-Terraform Infra includes Enviornment based deploymet. 
-
-Terraform Production 
-Terraform Staging 
--- module/instance.tf
+++
@@ -1,8 +0,0 @@
-
-resource "aws_instance" "redhat-instance" {
-  count         = var.instance_count
-  ami           = var.ami
-  instance_type = var.instance_type
-  tags          = merge(map("Name", "ubuntu-vm-${format("%02d", count.index + 1)}"), var.tags)
-}
-
