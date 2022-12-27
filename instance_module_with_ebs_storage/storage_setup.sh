5ffdaa0bfcf988f5670f01dafb9646dfb2c89652
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-27 15:45:45 +0500

    test

--- instance_module_with_ebs_storage/storage_setup.sh
+++ instance_module_with_ebs_storage/storage_setup.sh
@@ -1,7 +1,7 @@
 # #!/bin/bash
 # Set up the storage drive
-sudo mkfs.ext4 /dev/xvdg
+sudo mkfs.ext4 /dev/nvme1n1
 mkdir -p /app-data
-sudo e2label /dev/xvdg app-data
+sudo e2label /dev/nvme1n1 app-data
 sudo echo "LABEL=app-data /app-data ext4 rw,noatime,nodiratime 0 0" >> /etc/fstab
 mount -a
