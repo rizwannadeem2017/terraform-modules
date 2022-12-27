3edb418649ba6401aeb468077d29da4486ee1797
Rizwan Nadeem <rizwannadeem@rizwannadeems-MacBook-Pro.local>
2022-12-27 15:26:27 +0500

    test

--- instance_module_with_ebs_storage/ubuntu-vm.tf
+++ instance_module_with_ebs_storage/ubuntu-vm.tf
@@ -68,7 +68,7 @@ resource "aws_instance" "ubuntu-instance" {
     type        = "ssh"
     user        = "ubuntu"
     agent       = false
-    private_key = var.private_key
+    private_key = file(auth_key)
     timeout     = "3"
   }
 
